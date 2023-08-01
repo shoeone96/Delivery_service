1737481707cf9f79cb46b443ad054294300eb52c
shoeone96 <dlwnddnjs96@gmail.com>
2023-08-01 20:25:37 +0900

    #8 사장 로그인 기능 테스트 코드 작성
    로그인 관련 시나리오 test 코드 작성 및 성공

--- api-server-spring-boot-jdbc/src/test/java/com/example/demo/src/owner/OwnerControllerTest.java
+++ api-server-spring-boot-jdbc/src/test/java/com/example/demo/src/owner/OwnerControllerTest.java
@@ -1,7 +1,10 @@
 package com.example.demo.src.owner;
 
 import com.example.demo.config.BaseException;
+import com.example.demo.src.owner.requestDto.OwnerLoginRequestDto;
+import com.example.demo.src.owner.requestDto.OwnerRegisterRequestDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +12,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,7 +38,13 @@ public class OwnerControllerTest {
 
     @PersistenceContext
     EntityManager em;
+    @Autowired
+    private OwnerRepository ownerRepository;
 
+    @AfterEach
+    public void teardown() {
+        ownerRepository.deleteAll();
+    }
     @Test
     @DisplayName("사장등록 컨트롤러 테스트")
     @WithMockUser
@@ -59,7 +67,6 @@ public class OwnerControllerTest {
 
     @Test
     @DisplayName("동일 아이디 사장 등록 테스트")
-    @Rollback(true) // 롤백 설정 추가
     @Transactional
     void ownerExistNameRegisterTest() throws Exception {
 
@@ -80,6 +87,69 @@ public class OwnerControllerTest {
                 ).andDo(print())
                 .andExpect(status().isBadRequest())
                 .andExpect(result -> assertTrue(result.getResolvedException() instanceof BaseException));
+    }
+
+    @Test
+    @DisplayName("정상 사장 로그인 테스트")
+    @Transactional
+    void ownerLoginTest() throws Exception {
+
+        String username = "username";
+        String password = "password";
+        String name = "name";
+
+        ownerService.registerOwner(new OwnerRegisterRequestDto(username, password, name));
+        OwnerLoginRequestDto requestDto = new OwnerLoginRequestDto(username, password);
+
+        mockMvc.perform(post("/api/v1/owners/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(requestDto))
+                ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("사장 로그인 api 실패 테스트 - 유저 이름 잘못 기입")
+    @Transactional
+    void ownerLoginFailWrongUsernameTest() throws Exception {
+
+        String username = "username";
+        String password = "password";
+        String name = "name";
+
+        ownerService.registerOwner(new OwnerRegisterRequestDto(username, password, name));
+        OwnerLoginRequestDto requestDto = new OwnerLoginRequestDto(username+1, password);
+
+        mockMvc.perform(post("/api/v1/owners/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(requestDto))
+                ).andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof BaseException))
+                .andExpect(result -> assertEquals("유저가 존재하지 않습니다.", result.getResolvedException().getMessage()));;
+    }
+
+
+    @Test
+    @DisplayName("사장 로그인 api 실패 테스트 - 비밀번호 잘못 기입")
+    @Transactional
+    void ownerLoginFailWrongPasswordTest() throws Exception {
+
+        String username = "username";
+        String password = "password";
+        String name = "name";
+
+        ownerService.registerOwner(new OwnerRegisterRequestDto(username, password, name));
+        OwnerLoginRequestDto requestDto = new OwnerLoginRequestDto(username, password +1);
+
+        mockMvc.perform(post("/api/v1/owners/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(requestDto))
+                ).andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof BaseException))
+                .andExpect(result -> assertEquals("비밀번호가 유효하지 않습니다.", result.getResolvedException().getMessage()));
+        ;
     }
 
 
