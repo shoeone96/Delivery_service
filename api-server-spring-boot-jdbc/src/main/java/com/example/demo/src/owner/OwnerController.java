e2e167ab71424256b949eeb3bce941d906169d5b
shoeone96 <dlwnddnjs96@gmail.com>
2023-08-01 17:42:17 +0900

    #8 OwnerController 수정
    @RequestBody를 작성하지 않아 실제 Dto 객체가 받아지지 않던 문제 해결

--- api-server-spring-boot-jdbc/src/main/java/com/example/demo/src/owner/OwnerController.java
+++ api-server-spring-boot-jdbc/src/main/java/com/example/demo/src/owner/OwnerController.java
@@ -3,6 +3,7 @@ package com.example.demo.src.owner;
 import com.example.demo.config.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,7 @@ public class OwnerController {
     private final OwnerService ownerService;
 
     @PostMapping("/api/v1/owners")
-    public BaseResponse<Void> ownerJoin(OwnerRegisterRequestDto requestDto){
+    public BaseResponse<Void> ownerJoin(@RequestBody OwnerRegisterRequestDto requestDto){
         ownerService.registerOwner(requestDto);
         return BaseResponse.success();
     }
