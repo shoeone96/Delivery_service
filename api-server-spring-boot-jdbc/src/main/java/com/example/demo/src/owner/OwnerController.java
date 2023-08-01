42e14d9db2ea340c5d27e5e7be8dcc4009afb638
shoeone96 <dlwnddnjs96@gmail.com>
2023-08-01 17:25:42 +0900

    #8 사장님 등록 api 작성

---
+++ api-server-spring-boot-jdbc/src/main/java/com/example/demo/src/owner/OwnerController.java
@@ -0,0 +1,19 @@
+package com.example.demo.src.owner;
+
+import com.example.demo.config.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    @PostMapping("/api/v1/owners")
+    public BaseResponse<Void> ownerJoin(OwnerRegisterRequestDto requestDto){
+        ownerService.registerOwner(requestDto);
+        return BaseResponse.success();
+    }
+}
