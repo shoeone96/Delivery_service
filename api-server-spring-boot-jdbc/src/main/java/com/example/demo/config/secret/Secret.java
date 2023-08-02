1e5b4d4be95591b444f9543d7a2237472920c3e1
shoeone96 <dlwnddnjs96@gmail.com>
2023-08-02 15:57:52 +0900

    #12 jwt key 제대로 받을 수 있게 변경
    spring bean 에 등록하고 주입받을 수 있게 설정

--- api-server-spring-boot-jdbc/src/main/java/com/example/demo/config/secret/Secret.java
+++ api-server-spring-boot-jdbc/src/main/java/com/example/demo/config/secret/Secret.java
@@ -1,12 +1,19 @@
 package com.example.demo.config.secret;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 // TODO: 해당 KEY 값들을 꼭 바꿔서 사용해주세요!
 // TODO: .gitignore에 추가하는거 앚지 마세요!
+@Component
+@Getter
 public class Secret {
 
-    @Value("jwt.key")
-    public static String JWT_SECRET_KEY;
+    @Value("${jwt.key}")
+    private String JWT_SECRET_KEY;
 
 }
